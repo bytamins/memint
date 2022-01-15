baef5900fb57733674e74af700c299c6df5834a0
Sebastian Stant <sebastianstant@gmail.com>
2022-01-15 15:57:45 -0500

    added footer component

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,13 @@
+import { ByMoralis } from "react-moralis";
+
+const Footer = () => {
+  return (
+    <div>
+      <ByMoralis width={300} />
+      <ByMoralis width={300} variant="dark" />
+      <ByMoralis width={300} variant="light" />
+    </div>
+  );
+};
+
+export default Footer;
